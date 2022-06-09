79c07b57c820c91c2037ef98b2d438d9a60c260c
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-09 16:03:32 +0800

    Update project-demo.py

--- project-demo.py
+++ project-demo.py
@@ -40,8 +40,8 @@ prediction_proba = clf.predict_proba(df)
 st.subheader('Class labels and their corresponding index number')
 st.write(iris.target_names)
   
-st.subheader('Prediction')
-#st.write(iris.target_names[prediction])
+#st.subheader('Prediction')
+st.write(iris.target_names[prediction])
 st.write(prediction)
 
 st.subheader('Prediction Probability')
