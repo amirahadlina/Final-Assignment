5422cd695e1b2f89761f18ef9794532445f43a61
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-09 15:05:19 +0800

    Update project-demo.py

--- project-demo.py
+++ project-demo.py
@@ -38,7 +38,10 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(['Iris_setosa','Iris_virginica','Iris_versicolor'])
+flower = {'setosa',
+        'virginica',
+        'versicolor'}
+features = pd.DataFrame(flower, index=[0])
 
 st.subheader('Prediction')
 #st.write(iris.target_names[prediction])
